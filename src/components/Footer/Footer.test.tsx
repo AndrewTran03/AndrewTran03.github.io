c21dd1bb24a8c7c64ff5e05790d997e4ae0b6717
AndrewTran03 <andrewtranva@gmail.com>
2023-07-15 10:45:53 -0400

    Initial Commit

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,8 @@
+
+import { render, screen } from "@testing-library/react";
+import Footer from "./Footer";
+
+test("Footer Test", () => {
+    render(<BrowserRender><Footer /></BrowserRender>);
+
+});
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+
+const Footer: React.FC = () => {
+    const str = "Footer Component Works";
+
+    const str2 = "Chris, you need to pay attention frfr!"
+    return (<>{str + str2}</>);
+};
+
+export default Footer;
